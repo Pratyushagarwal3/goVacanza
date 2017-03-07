648f7ef8a7b4480a149b2c4eef39d1973935abd3
PRATYUSH AGARWAL <pratyushagarwal3@gmail.com>
2017-03-07 16:58:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
  <link rel="stylesheet" href="a.css">
 <title>GOVACANZA </title></head>
 <body>
-<h1> NIT CALICUT
+ <h1> NIT CALICUT</h1>
+ <form>
+  <input type="button" value="login">
+ </form>
 </body>
 </html>
