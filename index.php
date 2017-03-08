d1e5433526e892afae2930407b4310f71c03df74
PRATYUSH AGARWAL <pratyushagarwal3@gmail.com>
2017-03-08 13:45:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -23,41 +23,8 @@
 							 <li><a href="#section-2">About</a></li>
 							 <li><a href="#section-3">Top tours</a></li>
 							 <li><a href="#section-4">Contact</a></li>
-							<?php
-								error_reporting(E_ALL);
-								include_once("glogin/config.php");
-								include_once("glogin/includes/functions.php");
-
-								if(isset($_REQUEST['code'])){
-									$gClient->authenticate();
-									$_SESSION['token'] = $gClient->getAccessToken();
-									header('Location: ' . filter_var($redirectUrl, FILTER_SANITIZE_URL));
-								}
-
-								if (isset($_SESSION['token'])) {
-									$gClient->setAccessToken($_SESSION['token']);
-								}
-
-								if ($gClient->getAccessToken()) {
-									echo "<script>alert('i got access token ');</script>";
-									$userProfile = $google_oauthV2->userinfo->get();
-									//DB Insert
-									$gUser = new Users();
-									$gUser->checkUser('google',$userProfile['id'],$userProfile['given_name'],$userProfile['family_name'],$userProfile['email'],$userProfile['gender'],$userProfile['locale'],$userProfile['link'],$userProfile['picture']);
-									$_SESSION['google_data'] = $userProfile; // Storing Google User Data in Session
-										header("location: account.php");
-									$_SESSION['token'] = $gClient->getAccessToken();
-								} else {
-									$authUrl = $gClient->createAuthUrl();
-								}
-
-								if(isset($authUrl)) {
-									echo '<li><a href="'.$authUrl.'">google login</a></li>';
-								} else {
-									echo '<a href="logout.php?logout">Logout</a>';
-								}
-
-								?>
+							 <li><a href="b.php">Login</a></li>
+							
 					
 							 
  <div class="clearfix"></div>
